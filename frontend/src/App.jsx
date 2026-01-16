080d34fbf2bd493208b6223e10215c6bfbe3dede
govindvats21 <govindvats11m@gmail.com>
2026-01-16 17:45:15 +0530

    guhijoj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,7 +39,7 @@ import { setSocket } from "./redux/userSlice";
 
 // --- GLOBAL CONFIGURATION ---
 axios.defaults.withCredentials = true;
-export const serverURL = "https://ecommerce-backend-kx5b.onrender.com";
+export const serverURL = "https://ecommerce-backend-imja.onrender.com";
 
 // 🔥 1. AXIOS INTERCEPTOR: Har request mein token chipkayega
 axios.interceptors.request.use((config) => {
--- frontend/src/hooks/useGetCurrentUser.jsx
+++ frontend/src/hooks/useGetCurrentUser.jsx
@@ -5,7 +5,7 @@ import { setUserData } from '../redux/userSlice';
 
 const useGetCurrentUser = () => {
   const dispatch = useDispatch();
-  const API_URL = "https://ecommerce-backend-kx5b.onrender.com";
+  const API_URL = "https://ecommerce-backend-imja.onrender.com";
 
   useEffect(() => {
     const fetchUser = async () => {
