b3cbb2057d32229e5e100d360eba744ca65d7784
govindvats21 <govindvats11m@gmail.com>
2026-01-19 11:02:03 +0530

    cc4c

--- frontend/src/hooks/useGetItemByCity.jsx
+++ frontend/src/hooks/useGetItemByCity.jsx
@@ -6,7 +6,7 @@ import { setAllItems, setItemsInMyCity } from '../redux/userSlice';
 const useGetItemsByCity = () => {
   const dispatch = useDispatch();
   // Loop todne ke liye URL direct likhein
-  const API_URL = "http://localhost:8000";
+  const API_URL = "https://e-commerce-backend-one-inky.vercel.app";
 
   useEffect(() => {
     const fetchItems = async () => {
